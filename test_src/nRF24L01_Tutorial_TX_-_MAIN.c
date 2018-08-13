d5095c9971bdcae29aa8cee02f788f79834fbbc0
AnthonyZi_Pi <AnthonyZi@users.noreply.github.com>
2018-08-13 12:22:11 +0200

    atmega8 test_src do not work -> atmega resets itself after first loop iteration (can be seen using twi-interface debug messages)

--- test_src/nRF24L01_Tutorial_TX_-_MAIN.c
+++ test_src/nRF24L01_Tutorial_TX_-_MAIN.c
@@ -17,104 +17,66 @@
 #include "wl_module.h"
 #include "nRF24L01.h"
 
-volatile uint8_t timercounter;
+#include <string.h>
+
+#include "twi.h"
+#include <util/delay.h>
 
 int main(void)
 {
-        uint8_t payload[wl_module_PAYLOAD];             //Array for Payload
-        uint8_t maincounter =0;
-        uint8_t k;
+    uint8_t payload[wl_module_PAYLOAD];             //Array for Payload
+    uint8_t maincounter =0;
+    uint8_t k;
 
-        wl_module_init();       //initialise nRF24L01+ Module
-        _delay_ms(50);          //wait for nRF24L01+ Module
-        sei();
+    twi_init(2,255); //prescaler 2, bitrate 255 -> ~1kHz SCL
+    char twi_string[32];
+    memset(twi_string, '\0',sizeof(twi_string));
+    strcpy(twi_string,"twi_initialised");
+    twi_write_data(0x73,(uint8_t*)twi_string, 32);
+    _delay_ms(2000);
 
-        wl_module_tx_config(wl_module_TX_NR_0);         //Config Module
 
-        //Timer aktivieren ATMEGA8
-        #if defined(__AVR_ATmega8__)
-        TCCR0 |= ( (1<<CS02) | (1<<CS00));              //Prescaler auf 1024 ATMEGA8
-        TIMSK |= ( (1<<TOIE0));                         //enable TOVF ATMEGA8
-        #endif // __AVR_ATmega8__
+    wl_module_init();       //initialise nRF24L01+ Module
+    memset(twi_string, '\0',sizeof(twi_string));
+    strcpy(twi_string,"wl_module_init finished");
+    twi_write_data(0x73,(uint8_t*)twi_string, 32);
+    _delay_ms(2000);
 
-        //Timer aktivieren ATMEGA88A
-        #if defined(__AVR_ATmega88A__)
-        TCCR0B |= ( (1<<CS02) | (1<<CS00));
-        TIMSK0 |= ( (1<<TOIE0));
-        #endif // __AVR_ATmega88A__
+    _delay_ms(50);          //wait for nRF24L01+ Module
+    sei();
 
-    DDRB |= 0b00000100;
+    wl_module_tx_config(wl_module_TX_NR_0);         //Config Module
+    memset(twi_string, '\0',sizeof(twi_string));
+    strcpy(twi_string,"wl_module tx confred");
+    twi_write_data(0x73,(uint8_t*)twi_string, 32);
+    _delay_ms(2000);
+
+    memset(twi_string, '\0',sizeof(twi_string));
+    strcpy(twi_string,"entering while(1)");
+    twi_write_data(0x73,(uint8_t*)twi_string, 32);
+    _delay_ms(2000);
+
     while(1)
     {
-                if (timercounter >= 30)                 //30 entspricht ~1 Sekunde bei 8MHz
-                        {
-                                timercounter = 0;
-                                PORTB ^= 0b00000100;
+        memset(twi_string, '\0',sizeof(twi_string));
+        strcpy(twi_string,"while");
+        twi_write_data(0x73,(uint8_t*)twi_string, 32);
 
-                                for (k=0; k<=wl_module_PAYLOAD-1; k++)
-                                {
-                                        payload[k] = k;
-                                }
+        for (k=0; k<=wl_module_PAYLOAD-1; k++)
+        {
+            payload[k] = k;
+        }
 
-                                payload[0] = maincounter;
-                                payload[1] = maincounter+1;
+        payload[0] = maincounter;
+        payload[1] = maincounter+1;
 
-                                wl_module_send(payload,wl_module_PAYLOAD);
+        wl_module_send(payload,wl_module_PAYLOAD);
 
-                                maincounter++;
-                                if (maincounter >250)
-                                {
-                                        maincounter = 0;
-                                }
-                        }
+        maincounter++;
+        if (maincounter >250)
+        {
+            maincounter = 0;
+        }
+        _delay_ms(5000);
     }
-}
-
-ISR(TIMER0_OVF_vect)
-{
-        timercounter++;
-}
-
-
-//Unterscheidung je nach verwendeten µC
-#if defined(__AVR_ATmega8__)
-ISR(INT0_vect)
-#endif // __AVR_ATmega8__
-#if defined(__AVR_ATmega88A__)
-ISR(INT0_vect)
-#endif // __AVR_ATmega88A__
-#if defined(__AVR_ATmega168__)
-ISR(PCINT2_vect)
-#endif // __AVR_ATmega168__
-// Interrupt handler
-{
-    uint8_t status;
-
-        // Read wl_module status
-        wl_module_CSN_lo;                               // Pull down chip select
-        status = spi_fast_shift(NOP);                   // Read status register
-        wl_module_CSN_hi;                               // Pull up chip select
-
-
-                if (status & (1<<TX_DS))                                                        // IRQ: Package has been sent
-                {
-                        wl_module_config_register(STATUS, (1<<TX_DS));                          //Clear Interrupt Bit
-                        PTX=0;
-                }
-
-                if (status & (1<<MAX_RT))                                                       // IRQ: Package has not been sent, send again
-                {
-                        wl_module_config_register(STATUS, (1<<MAX_RT));                         // Clear Interrupt Bit
-                        wl_module_CE_hi;                                                        // Start transmission
-                        _delay_us(10);
-                        wl_module_CE_lo;
-                }
-
-                if (status & (1<<TX_FULL))                                                      //TX_FIFO Full <-- this is not an IRQ
-                {
-                        wl_module_CSN_lo;                                                       // Pull down chip select
-                        spi_fast_shift(FLUSH_TX);                                               // Flush TX-FIFO
-                        wl_module_CSN_hi;                                                       // Pull up chip select
-                }
-
 }
